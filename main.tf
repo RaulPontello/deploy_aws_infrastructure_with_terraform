f4230a8f4181237fbfa69f6e099e76a2a9489343
Raul Viacelli Pontello <62081176+RaulPontello@users.noreply.github.com>
2024-05-12 10:32:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Terraform settings
+
 terraform {
   required_providers {
     aws = {
@@ -10,21 +12,29 @@ terraform {
 # Configure the AWS Provider
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-# Create S3 bucket
+# Create AWS EC2 instance
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ec2_ami           # Application machine imagine of my EC2 instance
+  instance_type = var.ec2_instance_type # Instance type of my EC2 instance
+  tags          = var.ec2_tags      # Tags to set on the EC2 instance
+}
+
+# Create AWS S3 bucket
 
 resource "aws_s3_bucket" "s3bucket" {
-    bucket = var.bucket_name
-    tags = var.tags
+  bucket = var.s3_bucket_name # bucket name
+  tags   = var.s3_tags     # bucket tags
 }
 
-# Upload all files in path into S3 bucket
+# Upload all files in path into AWS S3 bucket
 
-resource "aws_s3_bucket_object" "upload_files" {
-  bucket = aws_s3_bucket.s3bucket.id
-  for_each = fileset(var.files_directory, "**")
-  key = each.value
-  source = "${var.files_directory}/${each.value}"
+resource "aws_s3_object" "upload_files" {
+  bucket   = aws_s3_bucket.s3bucket.id          # bucket name (check aws_s3_bucket resource)
+  for_each = fileset(var.files_directory, "**") # loop inside path (** includes files and sub-folders)
+  key      = each.value
+  source   = "${var.files_directory}/${each.value}"
 }
