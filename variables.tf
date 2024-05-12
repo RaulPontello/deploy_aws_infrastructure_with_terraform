80520506d5223aa90737b56da80f366d5ba6b4b4
Raul Viacelli Pontello <62081176+RaulPontello@users.noreply.github.com>
2024-05-12 10:33:15 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,53 @@
+# Other variables
+
+variable "aws_region" {
+  description = "Region were tools will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+# Variables for AWS S3
+
 variable "files_directory" {
   description = "Path where files are located"
-  type    = string
-  default = "C:/Users/Engie/Desktop/all_files"
+  type        = string
+  default     = "C:/Users/Engie/Desktop/all_files"
 }
 
-variable "bucket_name" {
+variable "s3_bucket_name" {
   description = "Name of the s3 bucket. Must be unique."
   type        = string
-  default = "bronze-raul-pontello2"
+  default     = "bronze-raul-pontello"
 }
 
-variable "tags" {
+variable "s3_tags" {
   description = "Tags to set on the bucket."
   type        = map(string)
   default     = {
-                Name = "bronze_tag"
-                Environment = "dev"
-                }
+    Name        = "bronze_tag"
+    Environment = "dev"
+  }
+}
+
+# Variables for AWS EC2
+
+variable "ec2_ami" {
+  description = "Application machine imagine of my EC2 instance"
+  type        = string
+  default     = "ami-07caf09b362be10b8"
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type of my EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ec2_tags" {
+  description = "Tags to set on the EC2 instance"
+  type        = map(string)
+  default     = {
+    Name        = "ec2_tag"
+    Environment = "dev"
+  }
 }
