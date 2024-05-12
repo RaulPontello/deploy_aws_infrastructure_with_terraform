bba074db87f35403ef5583cad68108bb98383256
Raul Viacelli Pontello <62081176+RaulPontello@users.noreply.github.com>
2024-05-12 10:32:55 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,23 @@
+# AWS S3 outputs 
+
 output "bucket_name" {
-  value = aws_s3_bucket.s3bucket.bucket
+  description = "Bucket name of my S3 bucket"
+  value       = aws_s3_bucket.s3bucket.bucket
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.s3bucket.arn
+  description = "Bucket ARN of my S3 bucket"
+  value       = aws_s3_bucket.s3bucket.arn
+}
+
+# AWS EC2 outputs 
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2_instance.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2_instance.public_ip
 }
