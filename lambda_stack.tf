fcddaf2a1250ea2b612f03cfbbe13ee8c2e1946c
Raul Pontello <raul.pontello@external.engie.com>
2024-07-27 09:40:13 -0300

    Update

--- lambda_stack.tf
+++ lambda_stack.tf
@@ -6,7 +6,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "lambda_function" {
   filename         = data.archive_file.lambda_zip.output_path
-  function_name    = ${var.lambda_file}
+  function_name    = var.lambda_file
   role             = aws_iam_role.iam_for_lambda.arn
   source_code_hash = data.archive_file.lambda.output_base64sha256
   environment {
