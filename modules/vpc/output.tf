fbf492492a837fd2295317549568bc09b8b76408
Raul Pontello <raul.pontello@external.engie.com>
2024-10-05 15:35:41 -0300

    Update

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-  value       = var.create_custom_vpc ? aws_vpc.custom_vpc.id : "default_vpc"
+  value       = var.create_custom_vpc ? aws_vpc.custom_vpc[0].id : "default_vpc"
   description = "The ID of the VPC"
 }
 
@@ -9,6 +9,6 @@ output "subnet_ids" {
 }
 
 output "db_subnet_group_name" {
-  value       = var.create_custom_vpc ? ws_db_subnet_group.this.name : "default_vpc"
+  value       = var.create_custom_vpc ? aws_db_subnet_group.this[0].name : "default_vpc"
   description = "The ID of the VPC"
 }
