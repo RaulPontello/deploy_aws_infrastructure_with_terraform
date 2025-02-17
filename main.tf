cf43ed3c4705c9587fdd88360bfbbe9213cf4168
Raul Pontello <raul.pontello@external.engie.com>
2025-02-17 15:01:21 -0300

    Update

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ module "rds_instance" {
   rds_instance_engine                 = var.rds_instance_engine
   db_name                             = var.db_name
   db_username                         = var.db_username
-  db_password                         = var.db_password
   vpc_id                              = module.vpc.vpc_id
   db_subnet_group_name                = module.vpc.db_subnet_group_name
   depends_on                          = [module.vpc]
--- variables.tf
+++ variables.tf
@@ -26,14 +26,6 @@ variable "create_custom_vpc" {
   default     = true
 }
 
-# Variables for AWS S3
-
-variable "s3_bucket_name" {
-  description = "Name of the s3 bucket. Must be unique."
-  type        = string
-  default     = "terraform-bucket"
-}
-
 # Variables for AWS RDS
 
 variable "db_name" {
@@ -51,12 +43,6 @@ variable "db_username" {
   description = "Username for the master DB user."
   type        = string
   default     = "terraform_db" # The Master username must contain only letters (A-Z and a-z) and any of the following: +=,.@-_
-}
-
-variable "db_password" {
-  description = "Password of the database" 
-  type        = string
-  default     = "terraform_pw" # Minimum constraints: At least 8 printable ASCII characters. Can't contain any of the following symbols: / ' " @
 }
 
 variable "instance_class" {
