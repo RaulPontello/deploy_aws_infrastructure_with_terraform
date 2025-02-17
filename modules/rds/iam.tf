c31d4be3b6f6ee869e289d944bd0479c82690f47
Raul Pontello <raul.pontello@external.engie.com>
2025-02-17 13:50:43 -0300

    Update

--- modules/rds/iam.tf
+++ modules/rds/iam.tf
@@ -6,7 +6,6 @@ resource "aws_security_group" "this" {
   name        = "${var.prefix}-rds-security-group-${var.suffix}"
   description = "Security group for AWS RDS instance"
   vpc_id      = var.vpc_id
-  tags        = var.tags
   ingress {
     from_port   = 5432
     to_port     = 5432
