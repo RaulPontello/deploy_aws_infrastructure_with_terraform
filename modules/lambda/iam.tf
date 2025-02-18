4119da1408456db9e96c123e96a6a80228101c25
Raul Pontello <raul.pontello@external.engie.com>
2025-02-18 15:56:09 -0300

    Update

--- modules/lambda/iam.tf
+++ modules/lambda/iam.tf
@@ -68,18 +68,3 @@ resource "aws_iam_role_policy_attachment" "policy_attach" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
 }
-
-# Create Security Group for my AWS Lambda function
-# (only created if create_custom_vpc is true)
-
-resource "aws_security_group" "this" {
-  count  = var.create_custom_vpc ? 1 : 0 
-  vpc_id = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
