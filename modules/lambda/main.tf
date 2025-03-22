15220572665c7c2b7445f163a5bb8aea7bb66cc9
Raul Pontello <raul.pontello@external.engie.com>
2025-03-22 09:29:47 -0300

    Update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "this" {
   filename         = data.archive_file.this.output_path
   function_name    = "${local.function_name}"
   role             = aws_iam_role.this.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "${local.python_file_name}.lambda_handler"
   runtime          = "python3.12"
   source_code_hash = data.archive_file.this.output_base64sha256
   
