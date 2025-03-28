845c6658aa8a5900ebfb22a978922c8730e20085
Raul Pontello <raul.pontello@external.engie.com>
2025-03-28 15:12:49 -0300

    Update

--- modules/glue/iam.tf
+++ modules/glue/iam.tf
@@ -37,8 +37,8 @@ data "aws_iam_policy_document" "this" {
         ]
         effect    = "Allow"
         resources = [
-          aws_s3_bucket.this.bucket,
-          "${aws_s3_bucket.this.bucket}/*"
+          aws_s3_bucket.this.arn,
+          "${aws_s3_bucket.this.arn}/*"
 
         ]
       }
