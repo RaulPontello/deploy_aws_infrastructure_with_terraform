a3472c8d6b35a2510ad525479975db14e71d16c9
Raul Pontello <raul.pontello@external.engie.com>
2025-04-08 17:49:45 -0300

    Update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -28,30 +28,9 @@ resource "aws_subnet" "private_subnets" {
   cidr_block              = cidrsubnet(aws_vpc.custom_vpc[0].cidr_block, 4, count.index + 10)
   availability_zone       = var.availability_zones[count.index]
   tags = {
-    Name = "${var.prefix}-custom-vpc-public-subnet-${count.index}"
+    Name = "${var.prefix}-custom-vpc-private-subnet-${count.index}"
   }
 }
-
-# resource "aws_subnet" "public" {
-#   count             = var.create_custom_vpc ? 1 : 0
-#   vpc_id            = aws_vpc.custom_vpc[0].id
-#   cidr_block        = cidrsubnet(aws_vpc.custom_vpc[0].cidr_block, 4, 10)
-#   availability_zone = var.availability_zones[count.index]
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "${var.prefix}-custom-vpc-public-subnet"
-#   }
-# }
-
-# resource "aws_subnet" "private" {
-#   count             = var.create_custom_vpc ? 1 : 0
-#   vpc_id            = aws_vpc.custom_vpc[0].id
-#   cidr_block        = cidrsubnet(aws_vpc.custom_vpc[0].cidr_block, 4, 12)
-#   availability_zone = var.availability_zones[count.index]
-#   tags = {
-#     Name = "${var.prefix}-custom-vpc-private-subnet"
-#   }
-# }
 
 # DB Subnet Group (uses subnets created above)
 
@@ -90,6 +69,6 @@ resource "aws_route_table" "this" {
 
 resource "aws_route_table_association" "route_table_associations" {
   count          = var.create_custom_vpc ? 1 : 0
-  subnet_id      = aws_subnet.public_subnets[count.index].id
+  subnet_id      = aws_subnet.public_subnets[*].id
   route_table_id = aws_route_table.this[0].id
 }
