8f05c6ee9e2d98fe3215b61b04775d985af2ef4e
Raul Pontello <raul.pontello@external.engie.com>
2025-04-09 09:10:42 -0300

    Update

--- main.tf
+++ main.tf
@@ -9,30 +9,30 @@ module "vpc" {
   use_public_subnet = var.use_public_subnet
 }
 
-# module "rds_instance" {
-#   source                              = "./modules/rds" 
-#   prefix                              = local.prefix
-#   create_custom_vpc                   = var.create_custom_vpc
-#   use_public_subnet                   = var.use_public_subnet
-#   instance_class                      = var.instance_class
-#   database_name                       = var.database_name
-#   db_username                         = var.db_username
-#   vpc_id                              = module.vpc.vpc_id
-#   db_subnet_group_name                = module.vpc.db_subnet_group_name
-#   depends_on                          = [module.vpc]
-# }
+module "rds_instance" {
+  source                              = "./modules/rds" 
+  prefix                              = local.prefix
+  create_custom_vpc                   = var.create_custom_vpc
+  use_public_subnet                   = var.use_public_subnet
+  instance_class                      = var.instance_class
+  database_name                       = var.database_name
+  db_username                         = var.db_username
+  vpc_id                              = module.vpc.vpc_id
+  db_subnet_group_name                = module.vpc.db_subnet_group_name
+  depends_on                          = [module.vpc]
+}
 
-# module "glue_job" {
-#   source                          = "./modules/glue" 
-#   prefix                          = local.prefix
-#   aws_region                      = var.aws_region
-#   secret_name                     = module.rds_instance.secret_name
-#   database_name                   = var.database_name
-#   database_host                   = module.rds_instance.rds_endpoint
-#   create_custom_vpc               = var.create_custom_vpc
-#   python_file_name                = var.python_file_name
-#   rds_instance_secret_manager_arn = module.rds_instance.rds_instance_secret_manager_arn
-#   vpc_id                          = module.vpc.vpc_id
-#   #subnet_ids                      = module.vpc.subnet_ids
-#   depends_on                      = [module.vpc, module.rds_instance]
-# }
+module "glue_job" {
+  source                          = "./modules/glue" 
+  prefix                          = local.prefix
+  aws_region                      = var.aws_region
+  secret_name                     = module.rds_instance.secret_name
+  database_name                   = var.database_name
+  database_host                   = module.rds_instance.rds_endpoint
+  create_custom_vpc               = var.create_custom_vpc
+  python_file_name                = var.python_file_name
+  rds_instance_secret_manager_arn = module.rds_instance.rds_instance_secret_manager_arn
+  vpc_id                          = module.vpc.vpc_id
+  #subnet_ids                      = module.vpc.subnet_ids
+  depends_on                      = [module.vpc, module.rds_instance]
+}
